14112506a4ac5a0eaf00d7afdc6fc5002d0327bc
Dheawp57 <71883223+Dheawp57@users.noreply.github.com>
2022-06-01 16:23:33 +0700

    Update footer.php

--- parts/footer.php
+++ parts/footer.php
@@ -2,7 +2,7 @@
 <footer class="text-center text-lg-start bg-secondary text-muted fixed-bottom">
   <div class="text-center p-4 text-white">
     © 2022 Copyright:
-    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Pungut Buku</a>
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">PUBU</a>
   </div>
 </footer>
 <!-- Footer -->
