23dcf09345a92f732d6ce68c3dfad33888ad1d43
Dheawp57 <71883223+Dheawp57@users.noreply.github.com>
2022-06-01 16:23:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,20 +18,25 @@ require "config.php";
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style/styleIndex.css">
 
-    <title>Pungut Buku</title>
+    <title>PUBU</title>
 </head>
 
 <body>
-    <nav class="navbar bg-secondary">
+    <div class="navigasi">
+    <nav class="navbar bg-secandary">
         <div class="container-fluid">
-            <a href="index.php"><span class="navbar-brand mb-0 h1 text-light">PUBU</span></a>
+            <a href="index.php"><span class="navbar-brand mb-0 h1 text-secandary">PUBU</span></a>
         </div>
     </nav>
+    </div>
+
+
   <div class="d-flex align-items-center mt-5">
     <div class="container d-flex flex-column justify-content-center align-items-center text-center w-50">
-        <h1 class="cover-heading">Welcome to Pungut Buku</h1>
-        <p>Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+        <h1 class="cover-heading">Welcome to PUBU </h1>
+        <p>Get your favorite book here</p>
         <div class="btn-toolbar">
             <a href="login.php" class="btn btn-dark mr-3">Login</a>
             <a href="register.php"class="btn btn-dark">Register</a>
