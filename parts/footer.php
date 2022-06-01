c55b8de9ce60bbfd234fc388c00fa773b691ca45
Dheawp57 <71883223+Dheawp57@users.noreply.github.com>
2022-06-01 17:22:30 +0700

    Update footer.php

--- parts/footer.php
+++ parts/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="text-center text-lg-start bg-secondary text-muted fixed-bottom">
-  <div class="text-center p-4 text-white">
+  <div class="text-center p-4 text-light">
     © 2022 Copyright:
     <a class="text-reset fw-bold" href="https://mdbootstrap.com/">PUBU</a>
   </div>
