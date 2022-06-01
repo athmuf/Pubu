9a17f15b39c83e5591e07d32992ca172f57a3e4f
Dheawp57 <71883223+Dheawp57@users.noreply.github.com>
2022-06-01 16:23:37 +0700

    Update header.php

--- parts/header.php
+++ parts/header.php
@@ -14,7 +14,7 @@ require "config.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
 
-    <title>Pungut Buku</title>
+    <title>PUBU</title>
 </head>
 
 <body>
