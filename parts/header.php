59b14a08c4d7453df6a0b0971a2ad398ec766416
Dheawp57 <71883223+Dheawp57@users.noreply.github.com>
2022-06-01 17:22:33 +0700

    Update header.php

--- parts/header.php
+++ parts/header.php
@@ -13,26 +13,29 @@ require "config.php";
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style/styleHeader.css">
 
     <title>PUBU</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-dark navbar-expand-lg bg-secondary">
+    <div class="navigasi">
+    <nav class="navbar navbar-secandary navbar-expand-lg bg-secandary">
     <div class="container-fluid">
-        <a class="navbar-brand mb-0 h1 text-light" href="#">PUBU</a>
+        <a class="navbar-brand mb-0 h1 text-secandary" href="#">PUBU</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link active link-light" aria-current="page" href="#">Home</a>
-                <a class="nav-link link-light" href="./kategori.php">Kategori</a>
-                <a class="nav-link link-light" href="./upload.php">Upload</a>
-                <a class="nav-link link-light" href="./akun.php">Akun</a>
-                <a class="nav-link link-light" href="./about.php">Tentang PUBU</a>
+                <a class="nav-link active link-secandary" aria-current="page" href="#">Home</a>
+                <a class="nav-link link-secandary" href="./kategori.php">Kategori</a>
+                <a class="nav-link link-secandary" href="./upload.php">Upload</a>
+                <a class="nav-link link-secandary" href="./akun.php">Akun</a>
+                <a class="nav-link link-secandary" href="./about.php">Tentang PUBU</a>
             </div>
         </div>
     </div>
     </nav>
+    </div>
 <!-- Header -->
