c07a858c4351c39b7947480ab1b64d3baa37e518
ajielll <azrielarsyi@gmail.com>
2022-06-03 13:40:14 +0700

    Update home.php

--- home.php
+++ home.php
@@ -19,6 +19,19 @@ if( isset($_POST["cari"])) {
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style/styleRegister.css">
+    <title>Register | PUBU</title>
+</head>
 
 <div class="d-flex align-items-center mt-5 mb-3">
     <div class="container d-flex flex-column justify-content-center align-items-center text-center w-50">
