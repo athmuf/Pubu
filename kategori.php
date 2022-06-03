83d35fd8c44cd9e1cf803b758d0c427e1eca5c2e
ajielll <azrielarsyi@gmail.com>
2022-06-03 13:14:37 +0700

    Update kategori.php

--- kategori.php
+++ kategori.php
@@ -12,6 +12,21 @@ $result=mysqli_query($conn, 'SELECT kategori FROM kategori_buku');
 $kategori=mysqli_fetch_assoc($result);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style/styleRegister.css">
+    <title>Register | PUBU</title>
+</head>
+
 <div class="d-flex align-items-center mt-5">
     <div class="container align-items-center justify-content-center w-75">
         <h1 class="cover-heading text-center mb-5">Cari buku berdasarkan kategori</h1>
